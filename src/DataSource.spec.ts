2d3b28fc6a9c2544a5d049b91f85631626d0f5d2
Doug Broad <dbroad@osisoft.com>
2021-06-02 10:41:42 -0400

    Add community/docker

--- src/DataSource.spec.ts
+++ src/DataSource.spec.ts
@@ -4,13 +4,15 @@ import { SdsDataSource } from 'DataSource';
 import { SdsDataSourceOptions, SdsDataSourceType } from 'types';
 
 describe('SdsDataSource', () => {
-  const eds_port = 'PORT';
-  const ocs_url = 'URL';
-  const ocs_version = 'VERSION';
-  const ocs_tenant = 'TENANT';
-  const ocs_client = 'CLIENT';
+  const edsPort = 'PORT';
+  const ocsUrl = 'URL';
+  const ocsVersion = 'VERSION';
+  const ocsTenant = 'TENANT';
+  const ocsClient = 'CLIENT';
   const oauthPassThru = false;
   const namespace = 'NAMESPACE';
+  const ocsUseCommunity = false;
+  const ocsCommunity = 'COMMUNITY';
   const ocsSettings: DataSourceInstanceSettings<SdsDataSourceOptions> = {
     id: 0,
     uid: '',
@@ -19,28 +21,30 @@ describe('SdsDataSource', () => {
     meta: null as any,
     jsonData: {
       type: SdsDataSourceType.OCS,
-      eds_port,
-      ocs_url,
-      ocs_version,
-      ocs_tenant,
-      ocs_client,
+      edsPort: edsPort,
+      ocsUrl: ocsUrl,
+      ocsVersion: ocsVersion,
+      ocsTenant: ocsTenant,
+      ocsClient: ocsClient,
       oauthPassThru,
       namespace,
+      ocsUseCommunity: ocsUseCommunity,
+      ocsCommunity: ocsCommunity,
     },
   };
   const edsSettings = { ...ocsSettings, ...{ jsonData: { ...ocsSettings.jsonData, type: SdsDataSourceType.EDS } } };
   const backendSrv = {
-    datasourceRequest: () => new Promise(r => r),
+    datasourceRequest: () => new Promise((r) => r),
   };
 
   describe('constructor', () => {
     it('should use passed in data source information', () => {
       const datasource = new SdsDataSource(ocsSettings, backendSrv as any);
       expect(datasource.type).toEqual(SdsDataSourceType.OCS);
-      expect(datasource.eds_port).toEqual(eds_port);
-      expect(datasource.ocs_url).toEqual(ocs_url);
-      expect(datasource.ocs_version).toEqual(ocs_version);
-      expect(datasource.ocs_tenant).toEqual(ocs_tenant);
+      expect(datasource.edsPort).toEqual(edsPort);
+      expect(datasource.ocsUrl).toEqual(ocsUrl);
+      expect(datasource.ocsVersion).toEqual(ocsVersion);
+      expect(datasource.ocsTenant).toEqual(ocsTenant);
       expect(datasource.oauthPassThru).toEqual(oauthPassThru);
       expect(datasource.namespace).toEqual(namespace);
     });
@@ -61,7 +65,7 @@ describe('SdsDataSource', () => {
   });
 
   describe('query', () => {
-    it('should query with the expected parameters', done => {
+    it('should query with the expected parameters', (done) => {
       spyOn(backendSrv, 'datasourceRequest').and.returnValue(
         Promise.resolve({
           data: [
@@ -92,7 +96,8 @@ describe('SdsDataSource', () => {
           {
             refId: 'REFID',
             namespace: 'NAMESPACE',
-            stream: 'STREAM',
+            streamId: 'STREAM',
+            streamName: 'STREAM',
           },
         ],
       };
@@ -101,7 +106,7 @@ describe('SdsDataSource', () => {
         url: '/ocs/api/VERSION/tenants/TENANT/namespaces/NAMESPACE/streams/STREAM/data?startIndex=FROM&endIndex=TO',
         method: 'GET',
       });
-      response.then(r => {
+      response.then((r) => {
         expect(JSON.stringify(r)).toEqual(
           JSON.stringify({
             data: [
@@ -140,22 +145,22 @@ describe('SdsDataSource', () => {
   });
 
   describe('getStreams', () => {
-    it('should return empty if namespace is not defined', done => {
+    it('should return empty if namespace is not defined', (done) => {
       const datasource = new SdsDataSource(ocsSettings, backendSrv as any);
       datasource.namespace = '';
       const result = datasource.getStreams('');
-      result.then(r => {
+      result.then((r) => {
         expect(r).toEqual([]);
         done();
       });
     });
 
-    it('should query for streams', done => {
+    it('should query for streams', (done) => {
       const Id = 'Stream';
       spyOn(backendSrv, 'datasourceRequest').and.returnValue(Promise.resolve({ data: [{ Id }] }));
       const datasource = new SdsDataSource(ocsSettings, backendSrv as any);
       const result = datasource.getStreams('test');
-      result.then(r => {
+      result.then((r) => {
         expect(r).toEqual([{ value: Id, label: Id }]);
         done();
       });
@@ -163,7 +168,7 @@ describe('SdsDataSource', () => {
   });
 
   describe('testDatasource', () => {
-    it('should run a test query', done => {
+    it('should run a test query', (done) => {
       spyOn(backendSrv, 'datasourceRequest').and.returnValue(
         Promise.resolve({
           status: 200,
@@ -175,7 +180,7 @@ describe('SdsDataSource', () => {
         url: '/ocs/api/VERSION/tenants/TENANT/namespaces/NAMESPACE/streams',
         method: 'GET',
       });
-      response.then(r => {
+      response.then((r) => {
         expect(r).toEqual({
           status: 'success',
           message: 'Data source is working',
@@ -184,7 +189,7 @@ describe('SdsDataSource', () => {
       });
     });
 
-    it('should handle test failure', done => {
+    it('should handle test failure', (done) => {
       spyOn(backendSrv, 'datasourceRequest').and.returnValue(
         Promise.resolve({
           status: 400,
@@ -197,7 +202,7 @@ describe('SdsDataSource', () => {
         url: '/ocs/api/VERSION/tenants/TENANT/namespaces/NAMESPACE/streams',
         method: 'GET',
       });
-      response.then(r => {
+      response.then((r) => {
         expect(r).toEqual({
           status: 'error',
           message: '400: Error',
